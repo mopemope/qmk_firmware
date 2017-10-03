d91cf2c2fcd6aaf0ddd2760f1ae6e8af81cfa816
mopemope <yutaka.matsubara@gmail.com>
2017-10-03 19:47:08 +0900

    Use Leader key

--- keyboards/lets_split/keymaps/mopemope/keymap.c
+++ keyboards/lets_split/keymaps/mopemope/keymap.c
@@ -9,6 +9,7 @@ extern keymap_config_t keymap_config;
 #define _LOWER  1
 #define _RAISE  2
 #define _ADJUST 3
+#define _ARROW  4
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -25,6 +26,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  ARROW,
 };
 
 enum double_taps {
@@ -230,16 +232,103 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    /* SEQ_ONE_KEY(KC_SPC){ */
-    /*   register_code(KC_LCTL); */
-    /*   TAP(KC_SPC); */
-    /*   unregister_code(KC_LCTL); */
-    /* } */
-    /* SEQ_ONE_KEY(KC_Z){ */
-    /*   register_code(KC_LCTL); */
-    /*   TAP(KC_Z); */
-    /*   unregister_code(KC_LCTL); */
-    /* } */
+    // guake
+    SEQ_ONE_KEY(KC_G){
+      TAP(KC_F12);
+    }
+
+    // tmux prefix
+    SEQ_ONE_KEY(KC_Z){
+      register_code(KC_LCTL);
+      TAP(KC_Z);
+      unregister_code(KC_LCTL);
+    }
+
+    // Ctrl + C
+    SEQ_ONE_KEY(KC_C){
+      register_code(KC_LCTL);
+      TAP(KC_C);
+      unregister_code(KC_LCTL);
+    }
+
+    // Ctrl + V
+    SEQ_ONE_KEY(KC_V){
+      register_code(KC_LCTL);
+      TAP(KC_V);
+      unregister_code(KC_LCTL);
+    }
+
+    // Alt + W
+    SEQ_ONE_KEY(KC_W){
+      register_code(KC_LALT);
+      TAP(KC_W);
+      unregister_code(KC_LALT);
+    }
+
+    // HYPER
+    // HYPER + ↑
+    SEQ_ONE_KEY(KC_UP){
+      register_code(KC_LGUI);
+      TAP(KC_UP);
+      unregister_code(KC_LGUI);
+    }
+    // HYPER + ←
+    SEQ_ONE_KEY(KC_LEFT){
+      register_code(KC_LGUI);
+      TAP(KC_LEFT);
+      unregister_code(KC_LGUI);
+    }
+    // HYPER + →
+    SEQ_ONE_KEY(KC_RIGHT){
+      register_code(KC_LGUI);
+      TAP(KC_RIGHT);
+      unregister_code(KC_LGUI);
+    }
+    // HYPER + ↓
+    SEQ_ONE_KEY(KC_DOWN){
+      register_code(KC_LGUI);
+      TAP(KC_DOWN);
+      unregister_code(KC_LGUI);
+    }
+
+    // layer
+    // LOWER
+    SEQ_ONE_KEY(KC_L){
+      if (layer_tgl) {
+        layer_off(_LOWER);
+      } else {
+        layer_on(_LOWER);
+      }
+      layer_tgl = !layer_tgl;
+    }
+    // RAISE
+    SEQ_ONE_KEY(KC_R){
+      if (layer_tgl) {
+        layer_off(_RAISE);
+      } else {
+        layer_on(_RAISE);
+      }
+      layer_tgl = !layer_tgl;
+    }
+    // ADJUST
+    SEQ_ONE_KEY(KC_A){
+      if (layer_tgl) {
+        layer_off(_ADJUST);
+      } else {
+        layer_on(_ADJUST);
+      }
+      layer_tgl = !layer_tgl;
+    }
+
+    // henkan
+    SEQ_ONE_KEY(KC_H){
+      TAP(JP_HENK);
+    }
+
+    // muhenkan
+    SEQ_ONE_KEY(KC_M){
+      TAP(JP_MHEN);
+    }
 
   }
 }
@@ -247,27 +336,27 @@ void matrix_scan_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
    * ,-----------------------------------------, ,-----------------------------------------,
-   * | Esc  |  Q   |  W   |  E   |  R   |  T   | |  Y   |  U   |  I   |   O  |  P   | ;/+  |
+   * | Tab  |  Q   |  W   |  E   |  R   |  T   | |  Y   |  U   |  I   |   O  |  P   | ;/+  |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * | Tab  | A/AD |  S   |  D   |  F   |  G   | |  H   |  J   |  K   |   L  | @/AD |  :   |
+   * | Esc  | A/AD |  S   |  D   |  F   |  G   | |  H   |  J   |  K   |   L  | @/AR |  :   |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |-/Ctl |  Z   |  X   |  C   |  V   |  B   | |  N   |  M   |  ,<  |  .>  |  /?  |\/Ctl |
+   * |  -   |  Z   |  X   |  C   |  V   |  B   | |  N   |  M   |  ,<  |  .>  |  /?  |  \_  |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |  AD  | GUI  | Alt  | LEAD |Del/L |Sp/Sft| |En/Sft|Bksp/R| Left | Down |  Up  |Right |
+   * | LEAD | Ctl  | Alt  | GUI  |Del/L |Sp/Sft| |En/Sft|Bksp/R| Left | Down |  Up  |Right |
    * `-----------------------------------------' `-----------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-    KC_ESC,         KC_Q,             KC_W,    KC_E,    KC_R,              KC_T,          KC_Y,           KC_U,               KC_I,    KC_O,    KC_P,              JP_SCLN,        \
-    KC_TAB,         LT(_ADJUST,KC_A), KC_S,    KC_D,    KC_F,              KC_G,          KC_H,           KC_J,               KC_K,    KC_L,    LT(_ADJUST,JP_AT), JP_COLN,        \
-    CTL_T(KC_MINS), KC_Z,             KC_X,    KC_C,    KC_V,              KC_B,          KC_N,           KC_M,               KC_COMM, KC_DOT,  JP_SLSH,           CTL_T(JP_BSLS), \
-    TD(AD),         KC_LGUI,          KC_LALT, KC_LEAD, LT(_LOWER,KC_DEL), SFT_T(KC_SPC), SFT_T(KC_ENT),  LT(_RAISE,KC_BSPC), KC_LEFT, KC_DOWN, KC_UP,             KC_RIGHT        \
+    KC_TAB,  KC_Q,             KC_W,    KC_E,    KC_R,              KC_T,          KC_Y,           KC_U,               KC_I,    KC_O,    KC_P,             JP_SCLN,  \
+    KC_ESC,  LT(_ADJUST,KC_A), KC_S,    KC_D,    KC_F,              KC_G,          KC_H,           KC_J,               KC_K,    KC_L,    LT(_ARROW,JP_AT), JP_COLN,  \
+    KC_MINS, KC_Z,             KC_X,    KC_C,    KC_V,              KC_B,          KC_N,           KC_M,               KC_COMM, KC_DOT,  JP_SLSH,          JP_BSLS,  \
+    KC_LEAD, KC_LCTL,          KC_LALT, KC_LGUI, LT(_LOWER,KC_DEL), SFT_T(KC_SPC), SFT_T(KC_ENT),  LT(_RAISE,KC_BSPC), KC_LEFT, KC_DOWN, KC_UP,            KC_RIGHT  \
  ),
 
   /* Lower
    * ,-----------------------------------------, ,-----------------------------------------,
-   * | Zen  |   !  |   "  |   #  |   $  |   %  | |   &  |  '   |  -   |   (  |   )  | Del  |
+   * | Zen  |   !  |   "  |   #  |   $  |   %  | |   &  |  '   |  -   |   (  |   )  |  |   |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |      |   1  |   2  |   3  |   4  |   5  | |      |  ^   |  =   |   ;  |   :  |      |
+   * |      |   1  |   2  |   3  |   4  |   5  | |      |  ^   |  =   |   ;  |   :  |  yen |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
    * |      |   6  |   7  |   8  |   9  |   0  | |      |  ~   |      |   +  |   *  |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
@@ -275,15 +364,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------' `-----------------------------------------'
    */
   [_LOWER] = KEYMAP( \
-    JP_ZHTG, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT, JP_MINS, JP_LPRN, JP_RPRN, KC_DEL,  \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, JP_CIRC, JP_EQL,  JP_SCLN, JP_COLN, JP_BSLS, \
+    JP_ZHTG, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT, JP_MINS, JP_LPRN, JP_RPRN, JP_PIPE,  \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, JP_CIRC, JP_EQL,  JP_SCLN, JP_COLN, JP_YEN,  \
     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, JP_TILD, _______, JP_PLUS, JP_ASTR, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
 
   /* Raise
    * ,-----------------------------------------, ,-----------------------------------------.
-   * | Zen  |   1  |   2  |   3  |   4  |   5  | |   6  |   7  |   8  |   9  |   0  | Del  |
+   * | Zen  |   1  |   2  |   3  |   4  |   5  | |   6  |   7  |   8  |   9  |   0  |  |   |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
    * |      |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   (  |   )  |   [  |   ]  | yen  |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
@@ -294,7 +383,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_RAISE] = KEYMAP( \
-    JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+    JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_PIPE, \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   JP_LPRN, JP_RPRN, JP_LBRC, JP_RBRC, JP_YEN,  \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 , _______, _______, JP_LCBR, JP_RCBR, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
@@ -302,21 +391,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Adjust
    * ,-----------------------------------------, ,-----------------------------------------.
-   * | Esc  | PgDN | WhDn | MsUp | WhUp |WRKSP1| |WRKSP1| WhDn | MsUp | WhUp | PgUP | Del  |
+   * | Esc  |      | WhDn | MsUp | WhUp |WRKSP1| |      |      |      | PgDN | PgUP |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |Reset |      | MsLf | MsDn | MsRg |WRKSP2| |WRKSP2| MsLf | MsDn | MsRg |      |Reset |
+   * |Reset |      | MsLf | MsDn | MsRg |WRKSP2| |      |      |      |      |      |Reset |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |      |      |      |      | Copy |Paste | |Paste | Copy |      | Mute |VolDn |VolUp |
+   * |      |      |      |      | Copy |Paste | |      |      |      |      |      |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |      |      |      |      |Click2|Click1| |Click1|Click2|      |      |      |      |
+   * |      |      |      |      |Click2|Click1| |      |      |      |      |      |      |
    * `-----------------------------------------' `-----------------------------------------'
    */
 
   [_ADJUST] = KEYMAP( \
-    KC_ESC,  KC_PGDN, KC_WH_D, KC_MS_U, KC_WH_U,    WRKSP1,     WRKSP1,     KC_WH_D,    KC_MS_U, KC_WH_U, KC_PGUP, KC_DEL,  \
-    RESET,   _______, KC_MS_L, KC_MS_D, KC_MS_R,    WRKSP2,     WRKSP2,     KC_MS_L,    KC_MS_D, KC_MS_R, _______, RESET,   \
-    _______, _______, _______, _______, LCTL(KC_C), LCTL(KC_V), LCTL(KC_V), LCTL(KC_C), _______, KC_MUTE, KC_VOLD, KC_VOLU, \
-    _______, _______, _______, _______, KC_BTN2,    KC_BTN1,    KC_BTN1,    KC_BTN2,    _______, _______, _______, _______  \
+    KC_ESC,  _______, KC_WH_D, KC_MS_U, KC_WH_U,    WRKSP1,     _______, _______, _______, KC_PGDN, KC_PGUP, _______, \
+    RESET,   _______, KC_MS_L, KC_MS_D, KC_MS_R,    WRKSP2,     _______, _______, _______, _______, _______, RESET,   \
+    _______, _______, _______, _______, LCTL(KC_C), LCTL(KC_V), _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_BTN2,    KC_BTN1,    _______, _______, _______, _______, _______, _______  \
+  ),
+
+  /* Arrow
+   * ,-----------------------------------------, ,-----------------------------------------.
+   * | Esc  |      |      |      |      |      | |      | PgDN |  Up  | PgUp |      |      |
+   * |------+------+------+------+------+------| |------+------+------+------+------+------|
+   * |Reset |      |      |      |      |      | |      | Left | Down |Right |      |Reset |
+   * |------+------+------+------+------+------| |------+------+------+------+------+------|
+   * |      |      |      |      |      |      | |      |      |      |      |      |
+   * |------+------+------+------+------+------| |------+------+------+------+------+------|
+   * |      |      |      |      |      |      | |      |      |      |      |      |      |
+   * `-----------------------------------------' `-----------------------------------------'
+   */
+
+  [_ARROW] = KEYMAP( \
+    KC_ESC,  _______, _______, _______, _______, _______, _______, KC_PGDN, KC_UP,   KC_PGUP,  _______, _______,  \
+    RESET,   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, RESET,   \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______  \
   )
 };
 
