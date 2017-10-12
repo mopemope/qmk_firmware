7af37fc103b5fc9031b71a902d5e9212c93abace
mopemope <yutaka.matsubara@gmail.com>
2017-10-13 00:58:45 +0900

    Add undo shortcut

--- keyboards/lets_split/keymaps/mopemope/keymap.c
+++ keyboards/lets_split/keymaps/mopemope/keymap.c
@@ -32,6 +32,9 @@ extern keymap_config_t keymap_config;
 #define CS LCTL(KC_S)
 // C-c
 #define CC LCTL(KC_C)
+// C-/
+#define C_SL LCTL(JP_SLSH)
+
 // C-:
 #define CCOLN LCTL(JP_COLN)
 // C-;
@@ -274,14 +277,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
    * |-=/Ctl|  Z   |  X   |  C   |  V   |  B   | |  N   |  M   |  ,<  |  .>  |  /?  |  \   |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |  ^   |RAISE |  Alt | GUI  |Del/L |SP/SFT| |EN/SFT| BS/R | GUI  | Alt  |LOWER |  |   |
+   * |  ^   |EMACS |  ALT | GUI  |DEL/L |SP/SFT| |EN/SFT| BS/R | GUI  | ALT  |LOWER |  |   |
    * `-----------------------------------------' `-----------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-    KC_ESC,         KC_Q,             KC_W,    KC_E,    KC_R,              KC_T,          KC_Y,           KC_U,               KC_I,    KC_O,    KC_P,             JP_SCLN,        \
-    KC_TAB,         LT(_ADJUST,KC_A), KC_S,    KC_D,    KC_F,              KC_G,          KC_H,           KC_J,               KC_K,    KC_L,    LT(_EMACS,JP_AT), JP_COLN,        \
+    KC_ESC,         KC_Q,             KC_W,    KC_E,    KC_R,              KC_T,          KC_Y,           KC_U,               KC_I,    KC_O,    KC_P,             JP_SCLN, \
+    KC_TAB,         LT(_ADJUST,KC_A), KC_S,    KC_D,    KC_F,              KC_G,          KC_H,           KC_J,               KC_K,    KC_L,    LT(_EMACS,JP_AT), JP_COLN, \
     CTL_T(JP_MINS), KC_Z,             KC_X,    KC_C,    KC_V,              KC_B,          KC_N,           KC_M,               KC_COMM, KC_DOT,  JP_SLSH,          JP_BSLS, \
-    JP_CIRC,        MO(_RAISE),       KC_LALT, TD(GF),  LT(_LOWER,KC_DEL), SFT_T(KC_SPC), SFT_T(KC_ENT),  LT(_RAISE,KC_BSPC), TD(GF),  KC_RALT, MO(_LOWER),       JP_PIPE \
+    JP_CIRC,        MO(_EMACS),       KC_LALT, TD(GF),  LT(_LOWER,KC_DEL), SFT_T(KC_SPC), SFT_T(KC_ENT),  LT(_RAISE,KC_BSPC), TD(GF),  KC_RALT, MO(_LOWER),       JP_PIPE  \
  ),
 
   /* Lower
@@ -296,7 +299,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------' `-----------------------------------------'
    */
   [_LOWER] = KEYMAP( \
-    JP_ZHTG, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT, JP_MINS, JP_LPRN, JP_RPRN, JP_PIPE,  \
+    JP_ZHTG, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT, JP_MINS, JP_LPRN, JP_RPRN, JP_PIPE, \
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, JP_CIRC, JP_EQL,  JP_SCLN, JP_COLN, JP_YEN,  \
     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, JP_TILD, _______, JP_PLUS, JP_ASTR, _______, \
     _______, _______, _______, _______, _______, _______, _______, JP_MHEN, _______, _______, _______, _______  \
@@ -342,21 +345,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Emacs
    * ,-----------------------------------------, ,-----------------------------------------.
-   * |      |      |      |      |      |      | |      | PgDN |  Up  | PgUp |      |      |
+   * |      |      |      |      |      |      | |      |      |  Up  |      |      |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |      |  CA  |  CS  |      |      |      | |      | Left | Down |Right |      | CTRL |
+   * |      |  CA  |  CS  |      |      |      | |      | Left | Down |Right |      |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
    * |      |  CZ  |  CX  |  CC  |      |      | |      |      |      |      |      |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |RESET |      |      |      |DE/ALT|SP/CTL| |EN/CTL|BS/ALT|      |      |      |RESET |
+   * |RESET |      |      | C_SL |DE/ALT|SP/CTL| |EN/CTL|BS/ALT|      |      |      |RESET |
    * `-----------------------------------------' `-----------------------------------------'
    */
 
   [_EMACS] = KEYMAP( \
-    _______, _______, _______, _______, _______,       _______,       _______,       KC_PGDN,        KC_UP,   KC_PGUP,  _______, _______, \
-    _______, CA,      CS,      _______, _______,       _______,       _______,       KC_LEFT,        KC_DOWN, KC_RIGHT, _______, KC_LCTL, \
+    _______, _______, _______, _______, _______,       _______,       _______,       _______,        KC_UP,   _______,  _______, _______, \
+    _______, CA,      CS,      _______, _______,       _______,       _______,       KC_LEFT,        KC_DOWN, KC_RIGHT, _______, _______, \
     _______, CZ,      CX,      CC,      _______,       _______,       _______,       _______,        _______, _______,  _______, _______, \
-    RESET,   _______, _______, _______, ALT_T(KC_DEL), CTL_T(KC_SPC), CTL_T(KC_ENT), ALT_T(KC_BSPC), _______, _______,  _______, RESET    \
+    RESET,   _______, _______, C_SL,    ALT_T(KC_DEL), CTL_T(KC_SPC), CTL_T(KC_ENT), ALT_T(KC_BSPC), _______, _______,  _______, RESET    \
   )
 };
 
