3918bb22d34ca17074f0a4f8a0032237e8572001
mopemope <yutaka.matsubara@gmail.com>
2017-10-24 12:05:04 +0900

    Disable FORCE_HOLD

--- keyboards/lets_split/keymaps/mopemope/config.h
+++ keyboards/lets_split/keymaps/mopemope/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PREVENT_STUCK_MODIFIERS
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
+//#define TAPPING_FORCE_HOLD
 
 #undef MOUSEKEY_INTERVAL
 #define MOUSEKEY_INTERVAL 5
