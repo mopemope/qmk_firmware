2ebdc2f073cae85f474689dc8acfeab034564631
mopemope <yutaka.matsubara@gmail.com>
2017-11-15 17:37:33 +0900

    Use default

--- keyboards/lets_split/keymaps/mopemope/config.h
+++ keyboards/lets_split/keymaps/mopemope/config.h
@@ -31,10 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MASTER_LEFT
 
-#define PREVENT_STUCK_MODIFIERS
-#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
+/* #define PREVENT_STUCK_MODIFIERS */
+/* #define PERMISSIVE_HOLD */
+/* #define IGNORE_MOD_TAP_INTERRUPT */
+/* #define TAPPING_FORCE_HOLD */
+#define TAPPING_TERM 150
 
 #define MOUSEKEY_INTERVAL 5
 #define MOUSEKEY_TIME_TO_MAX 50
