081c26f4ab6db8bafffde7e19cb13b6466ca689a
mopemope <yutaka.matsubara@gmail.com>
2017-11-21 21:29:51 +0900

    Add hide and Ctrl+@

--- keyboards/lets_split/keymaps/mopemope/config.h
+++ keyboards/lets_split/keymaps/mopemope/config.h
@@ -31,9 +31,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MASTER_LEFT
 
-/* #define PREVENT_STUCK_MODIFIERS */
-/* #define PERMISSIVE_HOLD */
-/* #define IGNORE_MOD_TAP_INTERRUPT */
+#define PREVENT_STUCK_MODIFIERS
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 /* #define TAPPING_FORCE_HOLD */
 #define TAPPING_TERM 150
 
