726f6f288d323e9b1a187e8289346d040c657ec7
mopemope <yutaka.matsubara@gmail.com>
2018-01-04 01:26:56 +0900

    Add SFT

--- keyboards/minidox/keymaps/mopemope/keymap.c
+++ keyboards/minidox/keymaps/mopemope/keymap.c
@@ -185,6 +185,7 @@ extern keymap_config_t keymap_config;
 #define ZHTG   JP_ZHTG
 #define ALT    KC_LALT
 #define CTL    KC_LCTL
+#define SFT    KC_LSFT
 #define ESC    KC_ESC
 #define DC     DF(COLEMAK)
 #define DE     DF(EMACS)
@@ -229,45 +230,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* COLEMAK
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   F  |   P  |   G  |           |   J  |   L  |   U  |   Y  |   :  |
+ * |   Q  |   W  |   F  |   P  |   G  |           |   J  |   L  |   U  |   Y  | DEL  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   A  |   R  |   S  |   T  |   D  |           |   H  |   N  |   E  |   I  |   O  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |           |   K  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  | CTL  | DEL  |      |    |      |  BS  | ALT  |
+ *                  | CTL  | SFT  |      |    |      |  BS  | ALT  |
  *                  `-------------| SPC  |    | ENT  |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
  */
 [COLEMAK] = KEYMAP( \
-  KC_Q,   KC_W,    KC_F,   KC_P,    KC_G,        KC_J,    KC_L,    KC_U,   KC_Y,   COLN, \
+  KC_Q,   KC_W,    KC_F,   KC_P,    KC_G,        KC_J,    KC_L,    KC_U,   KC_Y,   DEL,  \
   A_M,    KC_R,    KC_S,   KC_T,    KC_D,        KC_H,    KC_N,    KC_E,   KC_I,   O_M,  \
   Z_A,    KC_X,    KC_C,   KC_V,    KC_B,        KC_K,    KC_M,    COMM,   DOT,    S_M,  \
-                   CTL,    SDEL,    SPC,         ENT,     SBSPC,   ALT                   \
+                   CTL,    SFT,     SPC,         ENT,     SBSPC,   ALT                   \
 ),
 
 /* EMACS(COLEMAK)
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   F  |   P  |   G  |           |   J  |   L  |   U  |   Y  |   :  |
+ * |   Q  |   W  |   F  |   P  |   G  |           |   J  |   L  |   U  |   Y  | DEL  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   A  |   R  |   S  |   T  |   D  |           |   H  |   N  |   E  |   I  |   O  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |           |   K  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  | CTL  | DEL  |      |    |      |  BS  | ALT  |
+ *                  | CTL  | SFT  |      |    |      |  BS  | ALT  |
  *                  `-------------| SPC  |    | ENT  |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
  */
 [EMACS] = KEYMAP( \
-  KC_Q,   KC_W,    KC_F,  KC_P,    KC_G,         KC_J,    KC_L,    KC_U,   KC_Y,  COLN, \
+  KC_Q,   KC_W,    KC_F,  KC_P,    KC_G,         KC_J,    KC_L,    KC_U,   KC_Y,  DEL, \
   A_M,    KC_R,    KC_S,  KC_T,    KC_D,         KC_H,    KC_N,    KC_E,   KC_I,  O_M,  \
   Z_A,    KC_X,    KC_C,  KC_V,    KC_B,         KC_K,    KC_M,    COMM,   DOT,   S_M,  \
-                   CTL,   SDEL,    SPC,          ENT,     EBSPC,   ALT                  \
+                   CTL,   SFT,     SPC,          ENT,     EBSPC,   ALT                  \
 ),
 
 /* EMACS2(COLEMAK Shortcut Layer)
@@ -302,7 +303,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |   (  |   )  |   \  |           |  ^   |   -  |   ;  |   .  |   +  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  |      |      |      |    |      |      |      |
+ *                  |      |      |      |    |      | DEL  |      |
  *                  `-------------|      |    |      |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
@@ -311,7 +312,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   EXLM,     DQT,     HASH,    DLR,      PERC,     AMPR,    QUOT,    GRV,    PIPE, YEN,  \
   ESC,      AT,      LCBR,    RCBR,     UNDS,     TILD,    EQL,     COLN,   SLSH, ASTR, \
   XXXXXXX,  XXXXXXX, LPRN,    RPRN,     BSLS,     CIRC,    MINS,    SCLN,   DOT,  PLUS, \
-                     _______, _______,  _______,  _______, _______, _______               \
+                     _______, _______,  _______,  _______, DEL,     _______             \
 ),
 
 /* RAISE (Num + Fn + Others)
@@ -324,7 +325,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   _  |  F7  |  F8  |  F9  |  F10 |           |  F11 |  F12 |   (  |   )  |   -  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  |      |      |      |    |      |      |      |
+ *                  |      |      |      |    |      | DEL  |      |
  *                  `-------------|      |    |      |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
@@ -333,7 +334,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,   KC_9,   KC_0, \
   TAB,     KC_F1,   KC_F2,   KC_F3,   KC_F4,        KC_F5,   KC_F6,   LBRC,   RBRC,   AT,   \
   UNDS,    KC_F7,   KC_F8,   KC_F9,   KC_F10,       KC_F11,  KC_F12,  LPRN,   RPRN,   MINS, \
-                    _______, _______, _______,      _______, _______, _______               \
+                    _______, _______, _______,      _______, DEL,     _______               \
 ),
 
 /* MISC (GUI)
