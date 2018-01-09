e9fb1ba7363b128341e7e51963fd136a11646702
mopemope <yutaka.matsubara@gmail.com>
2018-01-09 23:45:35 +0900

    Move ALT

--- keyboards/minidox/keymaps/mopemope/keymap.c
+++ keyboards/minidox/keymaps/mopemope/keymap.c
@@ -187,6 +187,7 @@ extern keymap_config_t keymap_config;
 #define CTL    OSM(MOD_LCTL)
 #define SFT1   SFT_T(JP_MHEN)
 #define SFT2   SFT_T(JP_HENK)
+#define SFT3   LT(EMACS2,JP_HENK)
 #define ESC    KC_ESC
 #define DC     DF(COLEMAK)
 #define DE     DF(EMACS)
@@ -270,7 +271,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,   KC_W,    KC_F,  KC_P,    KC_G,         KC_J,    KC_L,    KC_U,   KC_Y,  DEL,  \
   A_M,    KC_R,    KC_S,  KC_T,    KC_D,         KC_H,    KC_N,    KC_E,   KC_I,  O_M,  \
   Z_A,    KC_X,    KC_C,  KC_V,    KC_B,         KC_K,    KC_M,    COMM,   DOT,   S_M,  \
-                   SFT1,  CTL,     SPC,          ENT,     EBSPC,   SFT2                  \
+                   SFT1,  CTL,     SPC,          ENT,     ABSPC,   SFT3                  \
 ),
 
 /* EMACS2(COLEMAK Shortcut Layer)
