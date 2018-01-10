79463b65930d527e6172e8a37ea6a700217609c0
mopemope <yutaka.matsubara@gmail.com>
2018-01-10 21:42:01 +0900

    Fix mouse parameter

--- keyboards/minidox/keymaps/mopemope/config.h
+++ keyboards/minidox/keymaps/mopemope/config.h
@@ -37,13 +37,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* #define QMK_KEYS_PER_SCAN 4 */
 #define AUTO_SHIFT_TIMEOUT 130
 
-#define MOUSEKEY_INTERVAL 5
-#define MOUSEKEY_TIME_TO_MAX 50
-#define MOUSEKEY_MAX_SPEED 2
-#define MOUSEKEY_MOVE_DELTA 5
+#define MOUSEKEY_INTERVAL 10
+#define MOUSEKEY_TIME_TO_MAX 40
+#define MOUSEKEY_MAX_SPEED 3
 #define MOUSEKEY_DELAY 0
 
-#define MOUSEKEY_WHEEL_MAX_SPEED 2
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 200
+#define MOUSEKEY_WHEEL_MAX_SPEED 3
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 #endif
