644eaf4756860954ad146b03c70f69460cda86a8
mopemope <yutaka.matsubara@gmail.com>
2018-01-29 23:50:56 +0900

    Update

--- keyboards/minidox/keymaps/mopemope/config.h
+++ keyboards/minidox/keymaps/mopemope/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PREVENT_STUCK_MODIFIERS
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-/* #define TAPPING_FORCE_HOLD */
+#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 140
 /* #define QMK_KEYS_PER_SCAN 4 */
 #define AUTO_SHIFT_TIMEOUT 120
