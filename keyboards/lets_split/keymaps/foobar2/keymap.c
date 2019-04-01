094cf61dba2d94648f3c33da93c948701654c52d
mopemope <yutaka.matsubara@gmail.com>
2019-04-01 10:40:16 +0900

    add macro

--- keyboards/lets_split/keymaps/foobar2/keymap.c
+++ keyboards/lets_split/keymaps/foobar2/keymap.c
@@ -479,6 +479,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         SEND_STRING(SS_LCTRL("xs"));
       }
+      break;
+    case CXO:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("x") "o");
+      }
+      break;
   }
   return true;
 }
