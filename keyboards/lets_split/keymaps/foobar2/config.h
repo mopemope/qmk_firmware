e4191e9c971fd7a5ec669faf6934f771583b3f47
mopemope <yutaka.matsubara@gmail.com>
2019-11-26 10:03:34 +0900

    fix tapping term

--- keyboards/lets_split/keymaps/foobar2/config.h
+++ keyboards/lets_split/keymaps/foobar2/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 120
+#define TAPPING_TERM 160
 // #define AUTO_SHIFT_TIMEOUT 150
 #define MOUSEKEY_INTERVAL 10
 #define MOUSEKEY_TIME_TO_MAX 100
