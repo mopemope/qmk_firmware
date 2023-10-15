96fc1665ce76b3d7641d65200bea10a9d8e06382
Yutaka Matsubara <matsubara@abby.co.jp>
2023-10-16 07:32:46 +0900

    feat: update

--- keyboards/totem/keymaps/mopemope/config.h
+++ keyboards/totem/keymaps/mopemope/config.h
@@ -13,14 +13,14 @@ for more options.
 #define NO_ACTION_ONESHOT
 #define FORCE_NKRO
 // #define DEBOUNCE 3
-// #define LAYER_STATE_16BIT
-#define TAP_CODE_DELAY 0
+#define LAYER_STATE_16BIT
+#define TAP_CODE_DELAY 1
 #define TAP_HOLD_CAPS_DELAY 0
 #define USB_POLLING_INTERVAL_MS 1
 #define GPIO_INPUT_PIN_DELAY 0
 #define NO_MUSIC_MODE
 
-/* #define PREVENT_STUCK_MODIFIERS */
-/* #define PERMISSIVE_HOLD */
-/* #define TAPPING_FORCE_HOLD */
+#define PREVENT_STUCK_MODIFIERS
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
 /* #defineTAPPING_TERM 150 */
