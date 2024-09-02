486465a4975eb44008ac28a9b97a1484a46c9369
Yutaka Matsubara <matsubara@abby.co.jp>
2024-09-03 07:18:24 +0900

    update

--- keyboards/totem/config.h
+++ keyboards/totem/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-#define DEBOUNCE 14
+#define DEBOUNCE 16
 
 /* Serial settings */
 #define USE_SERIAL
