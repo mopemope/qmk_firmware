cbea97afac4a1d3678663dbd950600e58da5bb37
Yutaka Matsubara <matsubara@abby.co.jp>
2024-09-02 22:13:50 +0900

    update

--- keyboards/maple_computing/minidox/keymaps/mopemope/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/mopemope/keymap.c
@@ -360,10 +360,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [DEAD] = LAYOUT(                                                        \
-  TAB,    KC_Q,   KC_W,   KC_E,   KC_R,        KC_1,   KC_2,   KC_3,   KC_4,   ESC,     \
-  SFT,    KC_A,   KC_S,   KC_D,   KC_F,        XXXXX,  XXXXX,  XXXXX,  XXXXX,  XXXXX,     \
-  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,        XXXXX,  XXXXX,  XXXXX,  XXXXX,  DQG,   \
-                  GALT,   GCTL,   KC_SPC,      KC_G,   KC_J,   KC_T                      \
+  TAB,    KC_Q,   KC_W,   KC_1,   KC_R,        KC_1,   KC_2,   KC_3,   KC_4,   ESC,     \
+  SFT,    KC_A,   KC_S,   KC_D,   KC_F,        KC_G,   XXXXX,  XXXXX,  XXXXX,  XXXXX,     \
+  ESC,    KC_2,   KC_3,   KC_4,   KC_B,        KC_Z,   KC_X,   KC_C,   KC_V,  DQG,   \
+                  GALT,   GCTL,   KC_SPC,      KC_B,   KC_J,   KC_T                      \
 ),
 
 [P3R] = LAYOUT(                                                         \
