4ee5e89ab1015c3defdbf5bac20573a07b8403f2
Yutaka Matsubara <matsubara@abby.co.jp>
2025-11-07 08:41:29 +0900

    chore(totem): adjust debounce and split USB timeout
    - Decrease DEBOUNCE from 18 to 16 to reduce input latency while preserving input stability.
    - Increase SPLIT_USB_TIMEOUT from 10000 to 15000 to improve reliability for split keyboard USB
    handling.
    - Other settings in keyboards/totem/config.h remain unchanged.

--- keyboards/totem/config.h
+++ keyboards/totem/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-#define DEBOUNCE 18
+#define DEBOUNCE 16
 
 /* Serial settings */
 #define USE_SERIAL
@@ -48,5 +48,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
-#define SPLIT_USB_TIMEOUT 10000
+#define SPLIT_USB_TIMEOUT 15000
 #define SPLIT_USB_TIMEOUT_POLL 20
